73aef86ea482a25fe02ff224d088014173c54ac0
Yevhen2mc2 <yevhen.o@2muchcoffee.com>
2021-11-23 15:47:17 +0200

    added some saga code and json-server

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import rootReducer from "./rootReducer";
 import createSagaMiddleware from "redux-saga";
+import { rootWatcher } from "./saga/watchers";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -9,5 +10,7 @@ const composeEnhancers =
 
 const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
 const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootWatcher);
 
 export default store;
---
+++ src/redux/saga/workers.ts
@@ -0,0 +1,8 @@
+export {};
+// function* postTaskToJSON(actions) {
+//   try {
+//     yield post({ type: "USER_FETCH_SUCCEEDED", user: user });
+//   } catch {
+//     throw new Error("Error post task to json-server");
+//   }
+// }
