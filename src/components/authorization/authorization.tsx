108efa434644c2741e6a9e9d51301639bb9fd4c5
Yevhen2mc2 <yevhen.o@2muchcoffee.com>
2021-11-29 15:56:39 +0200

    feat. done control password length and alert

--- src/components/authorization/authorization.tsx
+++ src/components/authorization/authorization.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./authorization.module.scss";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Form, Field } from "react-final-form";
-
 export const Authorization: React.FC = () => {
+  const PASSWORD_MIN_LENGTH: number = 8;
+
+  enum Styles {
+    PASSWORD_ALERT_OFF_STYLE = "passwordAlertOff",
+    PASSWORD_ALERT_ON_STYLE = "passwordAlertOn",
+  }
+
+  const [passwordAlertStyle, setPasswordAlertStyle] = useState<Styles>(
+    Styles.PASSWORD_ALERT_OFF_STYLE
+  );
+
   const onSubmit = (e) => {
     console.log("submit", e);
+    if (e.password?.length < 8 || !e.password) {
+      setPasswordAlertStyle(Styles.PASSWORD_ALERT_ON_STYLE);
+      setTimeout(() => {
+        setPasswordAlertStyle(Styles.PASSWORD_ALERT_OFF_STYLE);
+      }, 3000);
+    }
+  };
+
+  const onChangeForms = (values) => {
+    // if (values.password?.length < 8 || values.password?.length) {
+    // } else {
+    // }
+    return {};
   };
 
   return (
     <>
       <h1>Login at first</h1>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} validate={(values) => onChangeForms(values)}>
         {(props) => (
           <form onSubmit={props.handleSubmit}>
             <div className={style.loginContainer}>
@@ -55,12 +78,10 @@ export const Authorization: React.FC = () => {
                   </Field>
                 </div>
               </div>
-              <Button
-                disabled={false}
-                variant="contained"
-                color="success"
-                type="submit"
-              >
+              <div className={style[passwordAlertStyle]}>
+                Минимум {PASSWORD_MIN_LENGTH} символов для пароля!
+              </div>
+              <Button variant="contained" color="success" type="submit">
                 Ok
               </Button>
             </div>
