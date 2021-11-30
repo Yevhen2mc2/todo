4226173d3431b0067e027c084793a091936eae2c
Yevhen2mc2 <yevhen.o@2muchcoffee.com>
2021-11-30 15:08:02 +0200

    feat. made remove local storage date on click Logout

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { localStorageAPI } from "../../localStorage/localStorage";
+import { useNavigate } from "react-router-dom";
 
 export const Head: React.FC = () => {
-  return <div>head...</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      head...
+      <button
+        onClick={() => {
+          localStorageAPI.logOut();
+          navigate("login");
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
