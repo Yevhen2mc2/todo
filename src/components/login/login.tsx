2e911049072db8ec512943fe11fd7d36b21320dc
Yevhen2mc2 <yevhen.o@2muchcoffee.com>
2021-12-01 15:56:43 +0200

    refactor. made validate by final form

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -12,109 +12,77 @@ import { User } from "../../redux/user/types/types";
 export const Login: React.FC = () => {
   const navigate = useNavigate();
   const PASSWORD_MIN_LENGTH: number = 8;
-  let validEmail: boolean = false;
-  let validPassword: boolean = false;
 
-  enum Styles {
-    PASSWORD_ERROR_OFF = "passwordErrorOff",
-    PASSWORD_ERROR_ON = "passwordErrorOn",
-    EMAIL_ERROR_OFF = "emailErrorOff",
-    EMAIL_ERROR_ON = "emailErrorOn",
-  }
+  const checkEmail = (value) => {
+    if (value) {
+      return isEmail(value) ? undefined : "enter correct email";
+    }
+    return "required";
+  };
 
-  const [passwordErrorStyle, setPasswordErrorStyle] = useState<Styles>(
-    Styles.PASSWORD_ERROR_OFF
-  );
-
-  const [emailErrorStyle, setEmailErrorStyle] = useState<Styles>(
-    Styles.EMAIL_ERROR_OFF
-  );
-
-  const isValidPassword = (p: string | undefined): boolean => {
-    return !!(p && p.length >= PASSWORD_MIN_LENGTH);
+  const isValidPassword = (p: string | undefined): string | undefined => {
+    if (!p) {
+      return "empty";
+    }
+    if (p.length < PASSWORD_MIN_LENGTH) return "shorted";
+    return undefined;
   };
 
   const onSubmit = (e) => {
-    if (e.email) validEmail = isEmail(e.email);
-    validPassword = isValidPassword(e.password);
-
-    if (!validEmail) {
-      setEmailErrorStyle(Styles.EMAIL_ERROR_ON);
-      setTimeout(() => {
-        setEmailErrorStyle(Styles.EMAIL_ERROR_OFF);
-      }, 3000);
-    }
-
-    if (!validPassword) {
-      setPasswordErrorStyle(Styles.PASSWORD_ERROR_ON);
-      setTimeout(() => {
-        setPasswordErrorStyle(Styles.PASSWORD_ERROR_OFF);
-      }, 3000);
-    }
-
-    if (validEmail && validPassword) {
-      console.log("success login");
-      const user: User = {
-        email: e.email,
-      };
-      localStorageAPI.setUser(user);
-      navigate(url.list);
-    }
+    const user: User = {
+      email: e.email,
+    };
+    localStorageAPI.setUser(user);
+    navigate(url.list);
   };
 
   return (
     <>
       <h1>Login at first</h1>
-      <Form
-        onSubmit={onSubmit}
-        validate={(values) => {
-          //console.log("validate:", values);
-          return {};
-        }}
-      >
+      <Form onSubmit={onSubmit}>
         {(props) => (
           <form onSubmit={props.handleSubmit}>
             <div className={style.loginContainer}>
               <div className={style.containerEmail}>
                 <div className={style.textEmail}>Input your email:</div>
                 <div className={style.inputEmail}>
-                  <Field name="email">
-                    {(props) => (
-                      <TextField
-                        label="email"
-                        variant="outlined"
-                        type="text"
-                        name={props.input.name}
-                        value={props.input.value}
-                        onChange={props.input.onChange}
-                      />
+                  <Field name="email" validate={checkEmail}>
+                    {({ input, meta }) => (
+                      <>
+                        <TextField
+                          label="email"
+                          variant="outlined"
+                          type="text"
+                          name={input.name}
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {meta.error && meta.touched && <div>{meta.error}</div>}
+                      </>
                     )}
                   </Field>
-                  <div className={style[emailErrorStyle]}>
-                    Введите корректный email!
-                  </div>
                 </div>
               </div>
 
               <div className={style.containerPassword}>
                 <div className={style.textPassword}>Input your password:</div>
                 <div className={style.inputPassword}>
-                  <Field name="password">
-                    {(props) => (
-                      <TextField
-                        label="password"
-                        variant="outlined"
-                        type="password"
-                        name={props.input.name}
-                        value={props.input.value}
-                        onChange={props.input.onChange}
-                      />
+                  <Field name="password" validate={isValidPassword}>
+                    {({ input, meta }) => (
+                      <>
+                        <TextField
+                          label="password"
+                          variant="outlined"
+                          type="password"
+                          name={input.name}
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {meta.error && meta.touched && <div>{meta.error}</div>}
+                      </>
                     )}
                   </Field>
                 </div>
-              </div>
-              <div className={style[passwordErrorStyle]}>
-                Минимум {PASSWORD_MIN_LENGTH} символов для пароля!
               </div>
               <Button variant="contained" color="success" type="submit">
                 Ok
