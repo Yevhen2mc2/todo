a469e8ee25bce3711cd144907fadfe714f3fc9aa
Yevhen2mc2 <yevhen.o@2muchcoffee.com>
2021-12-06 12:04:54 +0200

    refactor. added types

--- src/redux/system/actions/actions.ts
+++ src/redux/system/actions/actions.ts
@@ -1,20 +1,20 @@
 import { actionsSystem } from "../types/types";
 
-export const setLoading = (value) => {
+export const setLoading = (value: boolean) => {
   return {
     type: actionsSystem.SET_LOADING,
     payload: value,
   };
 };
 
-export const setLoaded = (value) => {
+export const setLoaded = (value: boolean) => {
   return {
     type: actionsSystem.SET_LOADED,
     payload: value,
   };
 };
 
-export const setError = (value) => {
+export const setError = (value: boolean) => {
   return {
     type: actionsSystem.SET_ERROR,
     payload: value,
