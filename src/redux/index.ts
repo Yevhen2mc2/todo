4c6af4c9d57020df0487371af29ec4d3d4814a44
Yevhen2mc2 <yevhen.o@2muchcoffee.com>
2021-12-13 12:19:41 +0200

    feat. install toolkit

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,14 +1,11 @@
-import { applyMiddleware, createStore, compose } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 import createSagaMiddleware from "redux-saga";
 import { rootSaga } from "./saga/todo";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+const enhancer = applyMiddleware(sagaMiddleware);
 const store = createStore(rootReducer, enhancer);
 
 sagaMiddleware.run(rootSaga);
