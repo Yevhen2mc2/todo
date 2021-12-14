ecc7c54dcd67ebc9ed9966b6539e060b1d705d38
Yevhen2mc2 <yevhen.o@2muchcoffee.com>
2021-12-14 16:35:29 +0200

    added getTasksFromJson

--- src/shared/hooks.ts
+++ src/shared/hooks.ts
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { getTodoList } from "../redux/todo/selectors";
 import { AppDispatch, RootState } from "../redux";
+import { systemSlice } from "../redux/system/systemSlice";
+import { todoAPI } from "./API/todoAPI";
+import { ITaskItem, todoSlice } from "../redux/todo/todoSlice";
 
 export const useGetList = () => {
   const dispatch = useAppDispatch();
@@ -12,7 +15,19 @@ export const useGetList = () => {
   return useSelector(getTodoList);
 };
 
-const getTasksFromJson = () => () => {};
+const getTasksFromJson = () => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(systemSlice.actions.setLoading(true));
+    const data = await todoAPI.getAll();
+    const todos = await data.json();
+    dispatch(todoSlice.actions.putTaskToStore(todos));
+    dispatch(systemSlice.actions.setLoading(false));
+  } catch (e) {
+    dispatch(systemSlice.actions.setLoading(false));
+    dispatch(systemSlice.actions.setError("Error"));
+    throw new Error("Error get tasks from json-server");
+  }
+};
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
